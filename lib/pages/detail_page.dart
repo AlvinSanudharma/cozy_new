3013f0bb0d5e39ffa0638c627fdd87359eb1f7c7
AlvinSanudharma <alvinsanudharma@gmail.com>
2022-06-12 11:56:59 +0800

    Details Page - Image & Navigation

---
+++ lib/pages/detail_page.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+import '../theme.dart';
+
+class DetailPage extends StatelessWidget {
+  const DetailPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Stack(
+        children: [
+          Image.asset(
+            'assets/thumbnail.png',
+            width: MediaQuery.of(context).size.width,
+            height: 350,
+            fit: BoxFit.cover,
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: edge, vertical: 30),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                InkWell(
+                  onTap: (() {
+                    Navigator.pop(context);
+                  }),
+                  child: Image.asset(
+                    'assets/btn_back.png',
+                    width: 40,
+                  ),
+                ),
+                Image.asset(
+                  'assets/btn_wishlist.png',
+                  width: 40,
+                ),
+              ],
+            ),
+          )
+        ],
+      )),
+    );
+  }
+}
--- lib/widgets/space_card.dart
+++ lib/widgets/space_card.dart
@@ -1,86 +1,96 @@
 import 'package:cozy_new/models/Space.dart';
+import 'package:cozy_new/pages/detail_page.dart';
 import 'package:flutter/material.dart';
 import 'package:cozy_new/theme.dart';
 
 class SpaceCard extends StatelessWidget {
   final Space space;
 
-  const SpaceCard(this.space);
+  SpaceCard(this.space);
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        ClipRRect(
-          borderRadius: BorderRadius.circular(18),
-          child: Container(
-            width: 130,
-            height: 110,
-            child: Stack(children: [
-              Image.asset(
-                space.imageUrl ?? '',
-                width: 130,
-                height: 110,
-              ),
-              Align(
-                alignment: Alignment.topRight,
-                child: Container(
-                  width: 70,
-                  height: 30,
-                  decoration: BoxDecoration(
-                      color: purpleColor,
-                      borderRadius:
-                          BorderRadius.only(bottomLeft: Radius.circular(30))),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Image.asset(
-                        'assets/icon_star.png',
-                        width: 22,
-                      ),
-                      Text(
-                        '${space.rating}/5',
-                        style: whiteTextStyle.copyWith(fontSize: 13),
-                      )
-                    ],
-                  ),
+    return InkWell(
+      onTap: (() {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => DetailPage(),
+            ));
+      }),
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(18),
+            child: Container(
+              width: 130,
+              height: 110,
+              child: Stack(children: [
+                Image.asset(
+                  space.imageUrl!,
+                  width: 130,
+                  height: 110,
                 ),
-              )
-            ]),
+                Align(
+                  alignment: Alignment.topRight,
+                  child: Container(
+                    width: 70,
+                    height: 30,
+                    decoration: BoxDecoration(
+                        color: purpleColor,
+                        borderRadius:
+                            BorderRadius.only(bottomLeft: Radius.circular(30))),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Image.asset(
+                          'assets/icon_star.png',
+                          width: 22,
+                        ),
+                        Text(
+                          '${space.rating ?? 0}/5',
+                          style: whiteTextStyle.copyWith(fontSize: 13),
+                        )
+                      ],
+                    ),
+                  ),
+                )
+              ]),
+            ),
           ),
-        ),
-        SizedBox(
-          width: 20,
-        ),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              space.name ?? '',
-              style: blackTextStyle.copyWith(fontSize: 18),
-            ),
-            SizedBox(
-              height: 2,
-            ),
-            Text.rich(TextSpan(
-                text: '\$${space.price}',
-                style: purpleTextStyle.copyWith(fontSize: 16),
-                children: [
-                  TextSpan(
-                      text: ' / month',
-                      style: greyTextStyle.copyWith(fontSize: 16))
-                ])),
-            SizedBox(
-              height: 16,
-            ),
-            Text(
-              '${space.city}, ${space.country}',
-              style: greyTextStyle.copyWith(fontSize: 14),
-            )
-          ],
-        )
-      ],
+          SizedBox(
+            width: 20,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                space.name ?? '',
+                style: blackTextStyle.copyWith(fontSize: 18),
+              ),
+              SizedBox(
+                height: 2,
+              ),
+              Text.rich(TextSpan(
+                  text: '\$${space.price ?? 0}',
+                  style: purpleTextStyle.copyWith(fontSize: 16),
+                  children: [
+                    TextSpan(
+                        text: ' / month',
+                        style: greyTextStyle.copyWith(fontSize: 16))
+                  ])),
+              SizedBox(
+                height: 16,
+              ),
+              Text(
+                '${space.city ?? ''}, ${space.country ?? ''}',
+                style: greyTextStyle.copyWith(fontSize: 14),
+              )
+            ],
+          )
+        ],
+      ),
     );
   }
 }
